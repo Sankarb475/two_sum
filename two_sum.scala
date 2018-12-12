0af707bfffd48bccb1cce74c61b0e19ac5dbc1e1
Sankar Biswas <sankarb475@gmail.com>
2018-12-12 11:54:06 +0530

    Create two_sum.scala

---
+++ two_sum.scala
@@ -0,0 +1,17 @@
+
+'''
+Created on 12-Dec-2018
+
+@author: sankar.biswas
+'''
+
+object Solution {
+    def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+        for(a <- 0 until nums.length; b<- a+1 until nums.length) {
+                var sum = nums(a) + nums(b)
+                if(sum == target)
+                    return Array(a,b)
+        }
+        return Array(0,0)
+    }
+}
